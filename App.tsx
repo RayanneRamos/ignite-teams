6e0d290834a1a12cb5c57c56b532c1ffef6d5ce0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-10 16:30:06 -0300

    importando os tamanhos regular e bold das fontes na aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,23 @@
 import { Groups } from "./src/screens/Groups";
+import { ThemeProvider } from "styled-components";
+import { ActivityIndicator } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import theme from "./src/themes";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
+  );
 }
