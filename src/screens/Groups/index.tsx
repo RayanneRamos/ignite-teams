566146551c3ce70d18f055e3615dfaf5953f7b4d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-11 14:46:34 -0300

    renderizando a flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { GroupCard } from "../../components/GroupCard";
 import { Header } from "../../components/Header";
 import { Hightlight } from "../../components/Highlight";
 import { Container } from "./styles";
+import { FlatList } from "react-native";
+import { ListEmpty } from "../../components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Hightlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
