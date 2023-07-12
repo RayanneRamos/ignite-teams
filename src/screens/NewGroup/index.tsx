bc2fff94751de7d8aa26c38327ed9bfadc6301a9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-12 16:43:56 -0300

    fazendo as tratativas dos erros

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,6 +6,8 @@ import { Input } from "../../components/Input";
 import { Container, Content, Icon } from "./styles";
 import { useState } from "react";
 import { groupCreate } from "../../storage/group/groupCreate";
+import { AppError } from "../../utils/AppError";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const navigation = useNavigation();
@@ -13,11 +15,20 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
       await groupCreate(group);
 
       navigation.navigate("players", { group });
     } catch (error) {
-      throw error;
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
     }
   }
 
